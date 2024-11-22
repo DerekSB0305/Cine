c676c319cd9c6c2d559be1cf678cf11f1c9785c0
DerekSB0305 <dereksolorzano2111@gmail.com>
2024-11-22 09:11:23 -0600

    Pruebas tailwind

--- resources/views/admin/movies/index.blade.php
+++ resources/views/admin/movies/index.blade.php
@@ -4,31 +4,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @extends('layouts.app')
+@section('content')
     <title>Peliculas</title>
 </head>
 
 <body>
     <h1>Peliculas Administrador</h1>
     <a href="/admin/peliculas/crear">Crear Pelicula</a>
-    <table>
+    <table class="shadow-2xl table-auto border-separate border border-slate-400">
         <tr>
-            <th>Titulo</th>
-            <th>genero</th>
-            <th>duración</th>
-            <th>Clasificación</th>
-            <th>Sinopsis</th>
-            <th>imagen</th>
-            <th>Acciones</th>
+            <th class="border border-slate-800">Titulo</th>
+            <th class="border border-slate-800">genero</th>
+            <th class="border border-slate-800">duración</th>
+            <th class="border border-slate-800">Clasificación</th>
+            <th class="border border-slate-800">Sinopsis</th>
+            <th class="border border-slate-800">imagen</th>
+            <th class="border border-slate-800">Acciones</th>
         </tr>
 
         <tbody>
             @foreach ($movies as $movie)
             <tr>
-                <td>{{$movie->title}}</td>
-                <td>{{$movie->genre->name ?? 'Sin género' }}</td>
-                <td>{{$movie->duration}}</td>
-                <td>{{$movie->classification->name ?? 'Sin género'}}</td>
-                <td>{{$movie->synopsis}}</td>
+                <td class="border border-slate-800">{{$movie->title}}</td>
+                <td class="border border-slate-800">{{$movie->genre->name ?? 'Sin género' }}</td>
+                <td class="border border-slate-800">{{$movie->duration}}</td>
+                <td class="border border-slate-800">{{$movie->classification->name ?? 'Sin género'}}</td>
+                <td class="border border-slate-800">{{$movie->synopsis}}</td>
                 <td>
                     @if ($movie->image_url)
                     <img src="{{ asset('storage/' . str_replace('public/', '', $movie->image_url)) }}" alt="{{ $movie->title }}" width="100">
@@ -56,3 +58,4 @@
 </body>
 
 </html>
+@endsection
