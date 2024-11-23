7facc5471450830424ce65b62143e6d84009f4d4
DerekSB0305 <dereksolorzano2111@gmail.com>
2024-11-23 12:07:29 -0600

    index BD

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -47,7 +47,7 @@
               <div class="hidden sm:ml-6 sm:block">
                 <div class="flex space-x-4">
                   <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                  <a href="#" class="rounded-md bg-emerald-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Peliculas</a>
+                  <a href="/index" class="rounded-md bg-emerald-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Peliculas</a>
                   <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-emerald-900 hover:text-white">Proximamente</a>
                   <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-emerald-900 hover:text-white">Promos</a>
                   <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-emerald-900 hover:text-white">Alimentos</a>
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -22,7 +22,17 @@
    
  {{-- Boton Cartelera y proximamente --}}
 <div class="text-center">
-<x-buttons/>
+<!-- <x-buttons/> -->
+        @foreach($movies as $movie)
+            <div style="border: 1px solid #ccc; padding: 10px; width: 200px;">
+                <h2>{{ $movie->title }}</h2>
+                <img src="{{ asset('storage/' . str_replace('public/', '', $movie->image_url)) }}" alt="{{ $movie->title }}" style="width: 100%;">
+                <p><strong>Género:</strong> {{ $movie->genre->name ?? 'Sin género' }}</p>
+                <p><strong>Clasificación:</strong> {{ $movie->classification->name ?? 'Sin clasificación' }}</p>
+                <p><strong>Duración:</strong> {{ $movie->duration }}</p>
+                <p>{{ $movie->synopsis }}</p>
+            </div>
+        @endforeach
 </div>
      
    </main>
